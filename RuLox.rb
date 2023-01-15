c5c9c862c48fd22d2ed2efb9039ae01d502c04d2
Nicholas Turon <nicholas.turon@joinroot.com>
2023-01-15 15:16:39 -0500

    start of entry path

--- RuLox.rb
+++ RuLox.rb
@@ -1,1 +1,28 @@
-asdf
+class RuLox
+  def initialize(file_paths)
+    case file_paths.count
+    when 0
+      run_prompt
+    when 1
+      run_file(file_paths.first)
+    else
+      puts "Usage: rulox [script]"
+    end
+  end
+
+  def run_file(path)
+    puts "this is the file path"
+  end
+
+  def run_prompt
+    puts "now reading input..."
+    input = ''
+    until input == 'exit'
+      input = $stdin.gets
+
+      puts "> #{input}"
+    end
+  end
+end
+
+RuLox.new(ARGV)
