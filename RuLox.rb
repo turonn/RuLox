82e9688aeb80eaae1ea5eecbe59841b86802f9e4
turonn <72614966+turonn@users.noreply.github.com>
2023-01-15 17:06:11 -0500

    adds basic error handling and stubs Scanner class

--- RuLox.rb
+++ RuLox.rb
@@ -1,6 +1,10 @@
+require_relative 'scanner'
+
 class RuLox
   # @param args [Array[String]] command line arguments
   def initialize(args)
+    @had_error = false
+
     case args.count
     when 0
       run_prompt
@@ -19,23 +23,53 @@ class RuLox
     # raise some sort of error here if we cannot find the file
 
     run(file_contents)
+
+    # don't actually execute the file if there is an error in it
+    return if @had_error
   end
 
   def run_prompt
     puts "type 'exit' (without quotes) to exit"
     puts "now reading input..."
     while true 
-      input = $stdin.gets
-      break if input == "exit\n"
+      input = $stdin.gets.chomp
+      break if input == "exit"
 
       puts "> #{input}"
       run(input)
+
+      # reset error handling while in the interactive mode
+      # we don't want a single typed error to kill an
+      # interactive session
+      @had_error = false
     end
   end
 
   # @param source [String]
   def run(source)
-    puts source.inspect
+    puts "we are now in the run portion"
+
+    scanner = Scanner.new(source)
+    tokens = scanner.scan_tokens
+
+    tokens.each do |token|
+      puts token.inspect
+    end
+  end
+
+  # @param line [Integer]
+  # @param message [String]
+  def error(line, message)
+    report(line, "", message)
+  end
+
+  # @param line [Integer]
+  # @param where [String]
+  # @param message [String]
+  def report(line, where, message)
+    puts "[line #{line}] Error #{where} + #{message}"
+
+    @had_error = true
   end
 end
 
---
+++ Scanner.rb
@@ -0,0 +1,9 @@
+class Scanner
+  def initialize(source)
+
+  end
+
+  def scan_tokens
+    [1]
+  end
+end
