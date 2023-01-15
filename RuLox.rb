43a04cfc0059048d77c6e2edddaebb279f131e87
turonn <72614966+turonn@users.noreply.github.com>
2023-01-15 16:47:04 -0500

    adds the read_file path

--- RuLox.rb
+++ RuLox.rb
@@ -1,29 +1,41 @@
 class RuLox
-  def initialize(file_paths)
-    case file_paths.count
+  # @param args [Array[String]] command line arguments
+  def initialize(args)
+    case args.count
     when 0
       run_prompt
     when 1
-      run_file(file_paths.first)
+      run_file(args.first)
     else
       puts "Usage: rulox [script]"
     end
   end
 
-  def run_file(path)
+  # @param file_path [String]
+  def run_file(file_path)
     puts "this is the file path"
+    file_contents = File.open(file_path).read
+
+    # raise some sort of error here if we cannot find the file
+
+    run(file_contents)
   end
 
   def run_prompt
     puts "type 'exit' (without quotes) to exit"
     puts "now reading input..."
-    input = ''
     while true 
       input = $stdin.gets
       break if input == "exit\n"
 
       puts "> #{input}"
+      run(input)
     end
+  end
+
+  # @param source [String]
+  def run(source)
+    puts source.inspect
   end
 end
 
