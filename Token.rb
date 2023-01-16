60800f66c94b36ebc4409cb5fa909308386b2312
turonn <72614966+turonn@users.noreply.github.com>
2023-01-16 10:26:38 -0500

    basic token and token types

---
+++ Token.rb
@@ -0,0 +1,16 @@
+class Token
+  # @param type [TokenType]
+  # @param lexeme [String] how it's spelled
+  # @param literal [BasicObject] the actual thing
+  # @param line [Integer]
+  def initialize(type, lexeme, literal, line)
+    @type = type
+    @lexeme = lexeme
+    @literal = literal
+    @line = line
+  end
+
+  def to_string
+    type + ' ' + lexeme + ' ' + literal
+  end
+end
